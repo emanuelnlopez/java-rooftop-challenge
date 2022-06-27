ee69dcfef6338ac1727c19c07a8f10a84246979e
Emanuel López <emanuel.n.lopez@gmail.com>
2022-06-27 09:25:33 -0300

    Updates Main class

--- src/main/java/dev/rooftop/challenge/Main.java
+++ src/main/java/dev/rooftop/challenge/Main.java
@@ -1,7 +1,33 @@
 package dev.rooftop.challenge;
 
+import dev.rooftop.challenge.domain.model.SortBlocksException;
+import dev.rooftop.challenge.domain.service.SortBlocksServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try {
+            var service = new SortBlocksServiceImpl();
+
+            var token = service.getToken("emanuel.n.lopez@gmail.com");
+            System.out.println("Token obtenido: " + token);
+            System.out.println();
+
+            var blocks = service.getBlocks(token);
+            System.out.println("Bloques obtenidos:");
+            for (String block : blocks) {
+                System.out.println(" - " + block);
+            }
+            System.out.println();
+
+            var sortedBlocks = service.check(token, blocks);
+            System.out.println();
+            System.out.println("Bloques ordenados:");
+            for (String block : sortedBlocks) {
+                System.out.println(" - " + block);
+            }
+
+        } catch (SortBlocksException sbe) {
+            System.out.println("Ha ocurrido un error: " + sbe.getMessage());
+        }
     }
 }
